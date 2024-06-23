2f1c9169d1e89b4f5b87c6079d9c005bb2b53a00
SPetersNZ <137010917+SPetersNZ@users.noreply.github.com>
2024-06-24 09:38:49 +1200

    Performance enhancements Moved gridSize update call to its own function and changed call method to 'onchange' for improved performance

--- script.js
+++ script.js
@@ -1,25 +1,33 @@
 const slider = document.getElementById("sizeRange");
 const outputVertical = document.getElementById("sliderXVertical");
-const outputHorizonal = document.getElementById("sliderYHorizontal");
+const outputHorizontal = document.getElementById("sliderYHorizontal");
 const divParent = document.getElementById("divParent");
+document.getElementById("sizeRange").step = "16";
 
 outputVertical.innerText = slider.value;
-outputHorizonal.innerText = slider.value;
+outputHorizontal.innerText = slider.value;
 
 slider.oninput = function() {
+    outputVertical.innerText = this.value;
+    outputHorizontal.innerText = this.value;
+}
+
+slider.onchange = function() {
+    changeGridSize(this.value);
+    console.log(this.value);
+}
+
+function changeGridSize(gridSize) {
     while (divParent.hasChildNodes()) {
         divParent.removeChild(divParent.firstChild);
     }
-    outputVertical.innerText = this.value;
-    outputHorizonal.innerText = this.value;
-    console.log(this.value);
-    for (i = 0; i < this.value; i++) {
+    for (i = 0; i < gridSize; i++) {
         const divChild = document.createElement("div");
         divChild.setAttribute("id", "divChild"+[i]);
         document.getElementById("divParent").appendChild(divChild);
     }
-    for (i = 0; i < this.value; i++) {
-        for (j = 0; j < this.value; j++) {
+    for (i = 0; i < gridSize; i++) {
+        for (j = 0; j < gridSize; j++) {
             const divSubChild = document.createElement("div");
             divSubChild.setAttribute("id", "divSubChild");
             document.getElementById("divChild"+[i]).appendChild(divSubChild);
